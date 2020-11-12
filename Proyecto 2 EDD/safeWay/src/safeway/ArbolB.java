f292bae4627eb1803a0d51e5aba7f111e9363be7
cesarchs <58505340+cesarchs@users.noreply.github.com>
2020-11-11 19:29:49 -0600

    implemento arbol b

--- Proyecto 2 EDD/safeWay/src/safeway/ArbolB.java
+++ Proyecto 2 EDD/safeWay/src/safeway/ArbolB.java
@@ -3,8 +3,6 @@ package safeway;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  *
---
+++ Proyecto 2 EDD/safeWay/src/safeway/UsuarioNodoB.java
@@ -0,0 +1,29 @@
+
+package safeway;
+
+/**
+ *
+ * @author cesarchs
+ */
+public class UsuarioNodoB {
+    /* ESTE ES EL NODO PARA EL ARBOL B EN EL LLEVAREMOS EL CONTROL LOS USUARIOS */ 
+    public int id;
+    public String nombre;
+    public String usuario;
+    public String correo;
+    public String password;
+    public int telefono;
+    public int rolInt;
+    public String rolString;
+    
+    //constructor para crear usuarios
+    public UsuarioNodoB (int id, String nombre, String usuario, String correo, String password, int telefono, int rolint){
+        this.id = id;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.password = password;
+        this.telefono = telefono;
+        this.rolInt= rolint;
+    }
+}
